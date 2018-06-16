24e67099849b31411d5d2b2445302ad588b72a86
Daniel Bälz <me@dbaelz.de>
2018-06-16 13:07:08 +0200

    Add an example for the usage of the extension function

--- app/src/main/java/de/dbaelz/entwicklertag2018/demo/MainActivity.kt
+++ app/src/main/java/de/dbaelz/entwicklertag2018/demo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import de.dbaelz.entwicklertag2018.demo.user.User
 import de.dbaelz.entwicklertag2018.demo.user.UserService
+import de.dbaelz.entwicklertag2018.demo.user.hasMasterLevel
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun onButtonClicked() {
+    private fun onButtonClicked() {
         button_showUser.setOnClickListener {
             val username = editText_username.text.toString()
 
@@ -33,12 +34,17 @@ class MainActivity : AppCompatActivity() {
                     override fun onResponse(call: Call<User>, response: Response<User>?) {
                         Log.d("UserService.getUser", "onResponse -> $response - body -> ${response?.body()}")
 
-                        response?.body()?.let {
-                            cardView_username.text = it.username
-                            it.description?.isNotEmpty().apply {
-                                cardView_description.text = it.description
+                        response?.body()?.let { user ->
+                            cardView_username.text = user.username
+                            user.description?.isNotEmpty().apply {
+                                cardView_description.text = user.description
                             }
-                            cardView_points.text = getString(R.string.cardView_points_text, it.points)
+                            cardView_points.text = getString(R.string.cardView_points_text, user.points)
+
+                            if (user.hasMasterLevel()) {
+                                cardView_username.setTextColor(getColor(R.color.colorAccent))
+                                cardView_points.setTextColor(getColor(R.color.colorAccent))
+                            }
                         }
                     }
                 })
