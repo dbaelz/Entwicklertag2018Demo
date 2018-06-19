b884bc7af9ae2e46d1138b7bf1dee1a9c331448f
Daniel Bälz <me@dbaelz.de>
2018-06-19 16:15:22 +0200

    Show usage of backtick workaround for test method names

--- app/src/test/java/de/dbaelz/entwicklertag2018/demo/ExampleUnitTest.kt
+++ app/src/test/java/de/dbaelz/entwicklertag2018/demo/ExampleUnitTest.kt
@@ -11,7 +11,7 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun `2 plus 2 should still be 4`() {
         assertEquals(4, 2 + 2)
     }
 }
